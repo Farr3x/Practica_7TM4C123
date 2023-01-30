19ec51a978272550a5f9605e8a11fa4b155fa15c
unknown <farr98@gmail.com>
2023-01-29 20:49:00 -0600

    ya configurado

--- lib/include.h
+++ lib/include.h
@@ -15,9 +15,18 @@
 #define _25MHZ 0x0F
 #define _80MHZ 0x04 
 
+#define F100HZ (20000000/100)
+
+#define NVIC_ST_CTRL_COUNT      0x00010000  // Count flag
+#define NVIC_ST_CTRL_CLK_SRC    0x00000004  // Clock Source
+#define NVIC_ST_CTRL_INTEN      0x00000002  // Interrupt enable
+#define NVIC_ST_CTRL_ENABLE     0x00000001  // Counter mode
+#define NVIC_ST_RELOAD_M        0x00FFFFFF  // Counter load value
+
 #include "TM4C123GH6PM.h"
 #include "GPIO.h"
 #include "PLL.h"
 #include "UART.h"
+#include "ADC.h"
 
 #endif /* INCLUDE_H_ */
